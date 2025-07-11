97e9799646c58f9f90190ce906de4558493c8f81
AnymoreProject <anymore.dev13@gmail.com>
2025-07-11 10:22:44 +0000

    [BACKPORT] fs: implement SUSFS v1.5.9 There is a few change in here: - sus_mount functionality still remain in v1.5.5 as backporting it to the latest version will result a mount detection leak in some apps/detectors
    Thank you @simonpunk for the help of sus_path backport
    
    Co-authored-by: sidex15 <24408329+sidex15@users.noreply.github.com>
    Co-authored-by: simonpunk <simonpunk2016@gmail.com>
    Signed-off-by: @AnymoreProject <anymore.dev13@gmail.com>

--- kernel/sys.c
+++ kernel/sys.c
@@ -1182,6 +1182,10 @@ static int override_release(char __user *release, size_t len)
 	return ret;
 }
 
+#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
+	susfs_spoof_uname(&tmp);
+#endif
+
 SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 {
 	struct new_utsname tmp;
